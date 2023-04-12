938fe46af56c3a31a8c614adb24663cfcee286f3
YashwanthSwamy <yasw2875@colorado.edu>
2023-04-12 03:12:32 -0600

    fix: reverting changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Dashborad from './Dashboard/Dashboard';
+import LoginForm from './LoginForm/LoginForm';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+    <React.Fragment>
+        <meta name={"viewport"} content={"width=device-widt, initial scale=0.1"}></meta>
+                <BrowserRouter>
+                    <React.Fragment>
+                        <Switch>
+                            <Route path="" component={LoginForm} />
+                            <Route path="/dashborad" component={Dashborad} />
+                        </Switch>
+                    </React.Fragment>
+                </BrowserRouter>
+    </React.Fragment>
+    );
 }
 
 export default App;
