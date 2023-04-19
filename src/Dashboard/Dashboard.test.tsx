023e9b45e4df62326765318e66521a837f047821
PoojaNatarajan6548 <pona6548@colorado.edu>
2023-04-19 08:09:34 -0600

    add styling to dashboard

--- src/Dashboard/Dashboard.test.tsx
+++ src/Dashboard/Dashboard.test.tsx
@@ -1,4 +1,21 @@
-import { render, screen } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
+// import Dashboard from './Dashboard';
+
+// const mockedUsedNavigate = jest.fn();
+
+// jest.mock("react-router-dom", () => ({
+//   ...(jest.requireActual("react-router-dom") as any),
+//   useNavigate: () => mockedUsedNavigate
+// }));
+
+// test('renders', () => {
+//   render(<Dashboard />);
+//   const linkElement = screen.getByText(/Dashboard/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+import { render, fireEvent, screen } from '@testing-library/react';
 import Dashboard from './Dashboard';
 
 const mockedUsedNavigate = jest.fn();
@@ -8,8 +25,35 @@ jest.mock("react-router-dom", () => ({
   useNavigate: () => mockedUsedNavigate
 }));
 
-test('renders', () => {
-  render(<Dashboard />);
-  const linkElement = screen.getByText(/Dashboard/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Dashboard', () => {
+  test('renders', () => {
+    render(<Dashboard />);
+    const linkElement = screen.getByText(/Dashboard/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('displays recommended topics', () => {
+    render(<Dashboard />);
+    const topicsTitle = screen.getByText('Recommended Topics:');
+    expect(topicsTitle).toBeInTheDocument();
+
+    const topicsList = screen.getAllByRole('listitem');
+    expect(topicsList.length).toBeGreaterThan(0);
+  });
+
+  test('allows search for topics', () => {
+    render(<Dashboard />);
+    const searchTitle = screen.getByText('Find Topics:');
+    expect(searchTitle).toBeInTheDocument();
+
+    const searchInput = screen.getByLabelText('Search');
+    expect(searchInput).toBeInTheDocument();
+
+    const searchButton = screen.getByRole('button', { name: 'search' });
+    expect(searchButton).toBeInTheDocument();
+
+    fireEvent.change(searchInput, { target: { value: 'test' } });
+    fireEvent.click(searchButton);
+    // expect search functionality to be tested here
+  });
 });
--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -1,3 +1,194 @@
+// import Typography from "@mui/material/Typography";
+// import { useState } from 'react';
+// import {
+//   Button,
+//   Card,
+//   CardContent,
+//   TextField,
+// } from '@mui/material';
+// import NavBar from "../NavBar/NavBar";
+// import "./Dashboard.css";
+// import { Search } from "@mui/icons-material";
+// import { yellow } from "@mui/material/colors";
+
+// function Dashboard(): JSX.Element {
+//   const [searchText, setSearchText] = useState<string>('');
+
+//   const handleSearch = () => {
+//     // handle search logic here
+//   };
+
+//   return (
+//     <div>
+//       <NavBar />
+//       <Typography>
+//       <div>
+//         <Typography variant="h3" align="center" sx={{ mb: 4 }}>
+//           Welcome to Social Media Sentiment Analysis
+//         </Typography>
+//         <br></br>
+//         <div className="top-container">
+//         <Typography variant="h5" align="left" sx={{ mb: 2, mr: 10 }}>
+//             Recommended Topics:
+//         </Typography>
+//         </div>
+//         <div className="topics-container">
+//           <ul className="topics-list">
+//             <li>Topic 1</li>
+//             <li>Topic 2</li>
+//             <li>Topic 3</li>
+//             <li>Topic 1</li>
+//             <li>Topic 2</li>
+//             <li>Topic 3</li>
+//             <li>Topic 1</li>
+//             <li>Topic 2</li>
+//             <li>Topic 3</li>
+//           </ul>
+//         </div>
+//         <br></br>
+//         <div className="top-container">
+//         <Typography variant="h4" align="left" sx={{ mb: 2, mr: 10 }}>
+//             Find Topics:
+//         </Typography>
+//         </div>
+//         <div className="search-container">
+//           <TextField
+//             label="Search"
+//             variant="outlined"
+//             value={searchText}
+//             onChange={(event) => setSearchText(event.target.value)}
+//             InputProps={{
+//               endAdornment: (
+//                 <Button variant="contained" onClick={handleSearch}>
+//                   <Search />
+//                 </Button>
+//               ),
+//             }}
+//           />
+//         </div>
+//       </div>
+//       </Typography>
+//     </div>
+//   );
+// };
+
+// export default Dashboard;
+
+
+
+
+// import Typography from "@mui/material/Typography";
+// import { useState } from 'react';
+// import {
+//   Button,
+//   Card,
+//   CardContent,
+//   TextField,
+// } from '@mui/material';
+// import NavBar from "../NavBar/NavBar";
+// import "./Dashboard.css";
+// import { Search } from "@mui/icons-material";
+// import { yellow } from "@mui/material/colors";
+
+// function Dashboard(): JSX.Element {
+//   const [searchText, setSearchText] = useState<string>('');
+
+//   const handleSearch = () => {
+//     // handle search logic here
+//   };
+
+//   return (
+//     <div>
+//       <NavBar />
+//       <Typography>
+//         <div>
+//           <Typography variant="h3" align="center" sx={{ mb: 4 }}>
+//             Welcome to Social Media Sentiment Analysis
+//           </Typography>
+//           <br></br>
+//           <div className="top-container">
+//             <Typography variant="h5" align="left" sx={{ mb: 2, mr: 10 }}>
+//               Recommended Topics:
+//             </Typography>
+//           </div>
+//           <div className="topics-container">
+//             <div className="topics-grid">
+//               <div className="topic-item">
+//                 <div className="oval">Topic 1</div>
+//               </div>
+//               <div className="topic-item">
+//                 <div className="oval">Topic 2</div>
+//               </div>
+//               <div className="topic-item">
+//                 <div className="oval">Topic 3</div>
+//               </div>
+//               <div className="topic-item">
+//                 <div className="oval">Topic 4</div>
+//               </div>
+//               <div className="topic-item">
+//                 <div className="oval">Topic 5</div>
+//               </div>
+//               <div className="topic-item">
+//                 <div className="oval">Topic 6</div>
+//               </div>
+//               <div className="topic-item">
+//                 <div className="oval">Topic 7</div>
+//               </div>
+//               <div className="topic-item">
+//                 <div className="oval">Topic 8</div>
+//               </div>
+//               <div className="topic-item">
+//                 <div className="oval">Topic 9</div>
+//               </div>
+//               <div className="topic-item">
+//                 <div className="oval">Topic 10</div>
+//               </div>
+//               <div className="topic-item">
+//                 <div className="oval">Topic 11</div>
+//               </div>
+//               <div className="topic-item">
+//                 <div className="oval">Topic 12</div>
+//               </div>
+//               <div className="topic-item">
+//                 <div className="oval">Topic 13</div>
+//               </div>
+//               <div className="topic-item">
+//                 <div className="oval">Topic 14</div>
+//               </div>
+//               <div className="topic-item">
+//                 <div className="oval">Topic 15</div>
+//               </div>
+//             </div>
+//           </div>
+//           <br></br>
+//           <div className="top-container">
+//             <Typography variant="h4" align="left" sx={{ mb: 2, mr: 10 }}>
+//               Find Topics:
+//             </Typography>
+//           </div>
+//           <div className="search-container">
+//             <TextField
+//               label="Search"
+//               variant="outlined"
+//               value={searchText}
+//               onChange={(event) => setSearchText(event.target.value)}
+//               InputProps={{
+//                 endAdornment: (
+//                   <Button variant="contained" onClick={handleSearch}>
+//                     <Search />
+//                   </Button>
+//                 ),
+//               }}
+//             />
+//           </div>
+//         <div/>
+//       </Typography>
+//     </div>
+//   );
+// };
+
+// export default Dashboard;
+
 import Typography from "@mui/material/Typography";
 import { useState } from 'react';
 import {
@@ -9,6 +200,7 @@ import {
 import NavBar from "../NavBar/NavBar";
 import "./Dashboard.css";
 import { Search } from "@mui/icons-material";
+import { yellow } from "@mui/material/colors";
 
 function Dashboard(): JSX.Element {
   const [searchText, setSearchText] = useState<string>('');
@@ -20,26 +212,39 @@ function Dashboard(): JSX.Element {
   return (
     <div>
       <NavBar />
-      <div className="container">
+      <Typography>
+      <div>
         <Typography variant="h3" align="center" sx={{ mb: 4 }}>
           Welcome to Social Media Sentiment Analysis
         </Typography>
+        <br></br>
+        <div className="top-container">
+        <Typography variant="h5" align="center" sx={{ mb: 2, mr: 10 }}>
+            Recommended Topics:
+        </Typography>
+        </div>
         <div className="topics-container">
-          <ul className="topics-list">
-            <li>Topic 1</li>
-            <li>Topic 2</li>
-            <li>Topic 3</li>
-            <li>Topic 1</li>
-            <li>Topic 2</li>
-            <li>Topic 3</li>
-            <li>Topic 1</li>
-            <li>Topic 2</li>
-            <li>Topic 3</li>
-          </ul>
+          <div className="topics-grid">
+            <div className="topic-oval">Topic 1</div>
+            <div className="topic-oval">Topic 2</div>
+            <div className="topic-oval">Topic 3</div>
+            <div className="topic-oval">Topic 4</div>
+            <div className="topic-oval">Topic 5</div>
+            <div className="topic-oval">Topic 6</div>
+            <div className="topic-oval">Topic 7</div>
+            <div className="topic-oval">Topic 8</div>
+            <div className="topic-oval">Topic 9</div>
+            <div className="topic-oval">Topic 10</div>
+            <div className="topic-oval">Topic 11</div>
+            <div className="topic-oval">Topic 12</div>
+            {/* <div className="topic-oval">Topic 13</div>
+            <div className="topic-oval">Topic 14</div>
+            <div className="topic-oval">Topic 15</div> */}
+          </div>
         </div>
         <br></br>
         <div className="top-container">
-        <Typography variant="h4" align="left" sx={{ mb: 2, mr: 10 }}>
+        <Typography variant="h4" align="center" sx={{ mb: 2, mr: 10 }}>
             Find Topics:
         </Typography>
         </div>
@@ -58,7 +263,24 @@ function Dashboard(): JSX.Element {
             }}
           />
         </div>
+        <div className="topics-container">
+          <div className="topic-container">
+            <div className="topic-oval">Topic 1</div>
+            <div className="topic-oval">Topic 2</div>
+            <div className="topic-oval">Topic 3</div>
+            <div className="topic-oval">Topic 4</div>
+            <div className="topic-oval">Topic 5</div>
+            <div className="topic-oval">Topic 6</div>
+            <div className="topic-oval">Topic 7</div>
+            <div className="topic-oval">Topic 8</div>
+            <div className="topic-oval">Topic 9</div>
+            <div className="topic-oval">Topic 10</div>
+            <div className="topic-oval">Topic 11</div>
+            <div className="topic-oval">Topic 12</div>
+          </div>
+        </div>
       </div>
+      </Typography>
     </div>
   );
 };
